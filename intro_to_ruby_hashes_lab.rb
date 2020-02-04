f557cb66150ab2bac16b0a4e8deed5156d217d38
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 01:51:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
   :railroads => {
     :pieces => 4,
     :rent_in_dollars =>  {
-      :hi => 4,
+      :four_pieces_owned => 200,
       :three_pieces_owned => 100,
       :two_pieces_owned => 50,
       :one_piece_owned => 25

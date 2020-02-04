fb21a577293d3479eb449eb6768824bbd6c35f6f
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 01:55:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,9 @@ def monopoly_with_third_tier
       :b_and_o_railroad => {
         
       },
-      :NoWay => "open",
+      :shortline_railroad => {
+        
+      },
     }
   },
   :africa => {

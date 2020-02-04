b7d931aeaea303b6023cb40e201b13dae99fcefa
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 01:35:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,19 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  countries_and_capitals_of_the_world = {
+  :north_america => {
+    "Canada" => {
+      :capital => "Ottawa",
+      :capital_climate => "Kőppen Dfb"
+    },
+    "USA" => {
+      :capital => "Washington D.C.",
+      :capital_climate => "Kőppen Cfa"
+    }
+  }
+}
+ 
 end
 
 def monopoly_with_fourth_tier

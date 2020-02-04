56137a9486b729bd462e9f64e2ed65576eaaeaf9
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 01:30:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,9 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	states_and_capital_of_us = {
-	  
+	  "Maryland" => {
+	    
+	  }
 	}
 end
 
@@ -11,6 +13,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  states_and_capital_of_us = {
+	  "Maryland" =>{
+	    :capital => "Annapolis"
+	  }
+	}
 end
 
 def monopoly_with_third_tier

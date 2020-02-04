4baef350e71ecb1d70e9aea0d9440b083a112424
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-04 01:41:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,8 @@ def monopoly_with_second_tier
   # complexity.
   countries_and_capitals_of_the_world = {
   :railroads => {
-    :pieces => {
-      :piecces => "Ottawa",
-      :capital_climate => "Kőppen Dfb"
-    },
+    :pieces => 4
+    
     "USA" => {
       :capital => "Washington D.C.",
       :capital_climate => "Kőppen Cfa"
